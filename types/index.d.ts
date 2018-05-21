0aa16f8b3eaf71bbd8dbc0a8951558eab60939a4
singuerinc <nahuel.scotti@gmail.com>
2018-05-21 22:26:18 +0200

    feat: add statesByNumber, statesByCode types

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,5 +5,17 @@ declare const isFreeCall: (value: string) => boolean;
 declare const isLowCost: (value: string) => boolean;
 declare const isHighCost: (value: string) => boolean;
 declare const isOther: (value: string) => boolean;
+declare const statesByCode: (value: string) => string[];
+declare const statesByNumber: (value: string) => string[];
 
-export { isPhone, isFixed, isMobile, isFreeCall, isLowCost, isHighCost, isOther };
+export {
+  isPhone,
+  isFixed,
+  isMobile,
+  isFreeCall,
+  isLowCost,
+  isHighCost,
+  isOther,
+  statesByCode,
+  statesByNumber
+};
